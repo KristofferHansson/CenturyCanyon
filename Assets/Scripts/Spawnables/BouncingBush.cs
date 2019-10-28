4c415a77b4aedc9f4847bc6dc86e988cd586f91d
Chris <thechrishanson@yahoo.com>
2019-10-28 17:18:15 -0600

    Add bouncing bush launch functionality

---
+++ Assets/Scripts/Spawnables/BouncingBush.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingBush : MonoBehaviour
+{
+    Rigidbody prb = null;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("P2Capsule"))
+        {
+            prb = other.gameObject.transform.parent.gameObject.GetComponent<Rigidbody>();
+            Invoke("Launch", 0.4f);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.Equals("P2Capsule"))
+        {
+            prb = null;
+        }
+    }
+
+    private void Launch()
+    {
+        prb.AddForce(Vector3.up * 600.0f);
+    }
+}
