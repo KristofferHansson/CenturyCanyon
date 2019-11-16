76165da3b1217d27d3eb1a6bb188d845bfd48dff
Chris Huckabee <slimeballgamesofficial@gmail.com>
2019-11-16 15:33:03 -0700

    Started lv2 and added boulder behavior

---
+++ Assets/Scripts/BoulderSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderSpawn : MonoBehaviour
+{
+
+    private Vector3 Startpos;
+    public GameObject water;
+
+    private Rigidbody boulder;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Startpos = this.gameObject.transform.position;
+    }
+        
+        
+    void OnTriggerEnter(Collider col)
+    {
+ 
+        if (water.gameObject == col.gameObject)
+        {
+            transform.position = Startpos;
+            boulder.velocity = Vector3.zero;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
