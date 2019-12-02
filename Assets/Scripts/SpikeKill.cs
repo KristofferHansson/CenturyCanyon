e1340b214ff141b5a5e6d418d654d1691a04deba
Chris <47432857+Chris@users.noreply.github.com>
2019-12-01 21:09:01 -0700

    Add spike asset, reorganize

---
+++ Assets/Scripts/SpikeKill.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeKill : MonoBehaviour
+{
+    [SerializeField] private LevelScript lvl;
+
+    void Start()
+    {
+        if (lvl is null)
+            lvl = GameObject.Find("LevelMaster").GetComponent<LevelScript>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        bool playerIn = false;
+        string deadPlayerName = "";
+        if (other.gameObject.name.Equals("P1Capsule"))
+        {
+            playerIn = true;
+            deadPlayerName = "Past player";
+        }
+        else if (other.gameObject.name.Equals("P2Capsule"))
+        {
+            playerIn = true;
+            deadPlayerName = "Future player";
+        }
+        if (playerIn)
+        {
+            lvl.ReportFailure();
+            print(deadPlayerName + " has been impaled.");
+        }
+    }
+}
