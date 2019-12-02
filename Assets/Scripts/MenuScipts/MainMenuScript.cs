bfac377ce185019a79f22b3a63347ef6de5d93c7
ParimalRaghavan <42382731+ParimalRaghavan@users.noreply.github.com>
2019-12-01 21:47:41 -0700

    Adding Main Menu

---
+++ Assets/Scripts/MenuScipts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+   public void NewGame()
+    {
+        SceneManager.LoadScene("L01_VerticalSlice");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene("L01_VerticalSlice");
+    }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("L02_Level_two");
+    }
+
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene("L03_Level3");
+    }
+}
