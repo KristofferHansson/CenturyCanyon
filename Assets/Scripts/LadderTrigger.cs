53873ed3a5de670c92f0252265601e044fabcd26
Chris <47432857+Chris@users.noreply.github.com>
2019-12-01 18:50:32 -0700

    Create new level, adjust LevelMaster prefab to be better start for levels

--- Assets/Scripts/LadderTrigger.cs
+++ Assets/Scripts/LadderTrigger.cs
@@ -6,6 +6,12 @@ public class LadderTrigger : MonoBehaviour
 {
     [SerializeField] private LevelScript lvl;
 
+    void Start()
+    {
+        if (lvl is null)
+            lvl = GameObject.Find("LevelMaster").GetComponent<LevelScript>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name.Equals("P1Capsule") || other.gameObject.name.Equals("P2Capsule"))
