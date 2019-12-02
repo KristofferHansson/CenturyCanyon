7da48d10833160909cdac31e24666a567e1430d3
Chris <47432857+Chris@users.noreply.github.com>
2019-12-02 16:59:02 -0700

    Complete tutorial level, change black hole to last 8 seconds by default

---
+++ Assets/Scripts/LevelScripts/TutorialLevel/ChangeInstruction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeInstruction : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro text;
+    [SerializeField] private string newText;
+    private bool changed = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!changed && other.gameObject.name.Equals("P1Capsule") || other.gameObject.name.Equals("P2Capsule"))
+        {
+            text.text = newText;
+            changed = true;
+        }
+    }
+}
