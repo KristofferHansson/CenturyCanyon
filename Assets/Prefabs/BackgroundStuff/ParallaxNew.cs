d3251d9705eb8caae03ceb7924e11f9e3f2022b9
kRaikkonen <42619926+kRaikkonen@users.noreply.github.com>
2019-12-13 01:58:27 -0700

    New backgrounds

---
+++ Assets/Prefabs/BackgroundStuff/ParallaxNew.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxnew : MonoBehaviour
+{
+    public GameObject cam;
+    public float parallaxVFX;
+    private float length, startpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float dist = (cam.transform.position.x * parallaxVFX);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
