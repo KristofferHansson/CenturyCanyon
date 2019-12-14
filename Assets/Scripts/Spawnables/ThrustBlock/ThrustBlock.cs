ac85ba34b141ee1b5caa39e7ecc47cd45ef3d319
Chris <47432857+Chris@users.noreply.github.com>
2019-12-13 19:27:55 -0700

    Add thrustblock fading

--- Assets/Scripts/Spawnables/ThrustBlock/ThrustBlock.cs
+++ Assets/Scripts/Spawnables/ThrustBlock/ThrustBlock.cs
@@ -17,10 +17,18 @@ public class ThrustBlock : MonoBehaviour
 
     private Vector3 movement = Vector3.zero;
 
+    private Material m_Material;
+    private Color m_Color;
+    private float alpha = 1.0f;
+    private float time;
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        m_Material = GetComponent<Renderer>().material;
+        m_Color = m_Material.color;
+        time = Time.time;
         Invoke("setMovement", countdown);
     }
 
@@ -29,6 +37,10 @@ public class ThrustBlock : MonoBehaviour
     {
         //GetComponent<Rigidbody>().AddForce(force * movement * Time.deltaTime);
         GetComponent<Rigidbody>().velocity = force * movement;
+        m_Material.color = new Color(m_Color.r, m_Color.g, m_Color.b, alpha);
+        float total = countdown + destructionCountdown + movementDuration;
+        alpha = (total - (Time.time - time)) / total;
+        print(alpha);
     }
 
     private void setMovement()
